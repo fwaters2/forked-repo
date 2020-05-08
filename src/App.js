441ad73d5b1f3e28f68bb14a277c75411aa444c0
fwaters2 <forrestwaters@gmail.com>
2020-05-08 12:49:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Forked repo
+          Forked repo pull request
         </p>
         <a
           className="App-link"
