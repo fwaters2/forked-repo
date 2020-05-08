a6a44f9b62f70e08827bc1a72e6ec6a708c9db2c
forrestwaters <forrestwaters@gmail.com>
2020-05-08 12:38:38 +0800

    testing greeting and label

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit to test Fixed!
+          Test labels and greeting
         </p>
         <a
           className="App-link"
